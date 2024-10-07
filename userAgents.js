76422fa2e6b25222d96ed53f845d12c5cda6a462
ZamadaMohamed <asadnoh5@gmail.com>
2024-10-07 18:36:16 +0300

    Update userAgents.js

--- userAgents.js
+++ userAgents.js
@@ -81,3 +81,5 @@ const userAgents = [
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15',
       'Mozilla/5.0 (Linux; Android 5.1.1; SM-G532F Build/LMY48B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36',
 ];
+// Exporting userAgents as JSON
+JSON.stringify(userAgents);
